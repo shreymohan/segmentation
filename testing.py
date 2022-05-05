9f9a1c9f80d45fb0c96d7a7fe3b7a3dd38dea1cb
Shrey Mohan <mohan.shrey92@gmail.com>
2022-05-05 07:50:24 -0700

    Add files via upload

--- testing.py
+++ testing.py
@@ -15,11 +15,14 @@ import tensorflow as tf
 import random
 from random import shuffle
 
-test_dir=''
+test_dir='/home/shrey/data/val/images'
+model_path='./model/unet.h5'
+
+model = keras.models.load_model(model_path)
 
 test_files=os.listdir(test_dir)
 path = np.random.choice(test_files)
-raw = Image.open(path)
+raw = Image.open(os.path.join(test_dir,path))
 raw = np.array(raw.resize((256, 256)))/255.
 raw = raw[:,:,0:3]
 
@@ -35,5 +38,9 @@ msk[msk < 0.5] = 0
 #show the mask and the segmented image 
 combined = np.concatenate([raw, msk, raw* msk], axis = 1)
 plt.axis('off')
-plt.imshow(combined)
-plt.show()
+plt.imsave('./test_res.jpeg',combined)
+#mask = Image.fromarray(msk)
+#segment=Image.fromarray(raw*msk)
+#mask.save("./image_mask.jpeg")
+#segment.save('./image_seg.jpeg')
+#plt.show()
